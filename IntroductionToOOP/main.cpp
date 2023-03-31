a8f0536956bbfe3a3db6f661763a08fdb8926007
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2023-04-01 01:41:32 +0500

    Add project files.

---
+++ IntroductionToOOP/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Point // создаем структуру или класс новый 
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this ->x = x;
+	}
+	void set_y(double y)
+	{
+		this ->y = y;
+	}
+	// Метод (Methods)
+	double distance(Point other)
+	{
+		// this - это точка от которой мы считаем расстояние
+		// other - это вторая точка, (указанная точка)
+		double x_distance = this->x - other.x; 
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance; // возвращаем значение 
+	}
+
+};
+
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance; 
+}
+
+//#define STRUCT_POINT
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
+	// Members - члены 
+	int a; // объявление переменной "а" типа int 
+	Point A; // объявление переменной "А" типа Point 
+	// объявление объекта А типа Point 
+	// создание экземпляра А структуры 
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+
+	Point A; 
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y () << endl; 
+
+	Point B;
+	B.set_x(7);
+	B.set_y(8); 
+	cout << B.get_x() << "\t" << B.get_y() << endl; 
+
+	cout << "Расстояние от точки А до точки Б:" << A.distance(B) << endl; // вызываем от точки А до точки Б, или наоборот 
+	cout << "Расстояние от точки Б до точки А:" << B.distance(A) << endl; // вызываем от точки А до точки Б, или наоборот 
+	cout << "Расстояние от точки А до точки Б:" << distance(A,B) << endl; // вызываем от точки А до точки Б, или наоборот 
+	cout << "Расстояние от точки Б до точки А:" << distance(B,A) << endl; // вызываем от точки А до точки Б, или наоборот 
+
+}
